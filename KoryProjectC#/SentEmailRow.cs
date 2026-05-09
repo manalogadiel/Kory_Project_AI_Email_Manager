2992b6bb929a13167416576aa6e4233eee1a3bb3
manalogadiel <gadielmanalo19@gmail.com>
2026-05-10 02:34:07 +0800

    added new rows

---
+++ KoryProjectC#/SentEmailRow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoryProjectC_
+{
+    public partial class SentEmailRow : UserControl
+    {
+        public SentEmailRow()
+        {
+            InitializeComponent();
+        }
+
+        private void SentEmailRow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rowPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2CirclePictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
